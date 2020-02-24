242e2b324b4f10a99612f6739f1c5da061b61abe
yashvirsingh05 <yashvir.singh_cs18@gla.ac.in>
2020-02-24 19:45:11 +0530

    for loop

--- src/FunctionDefinition/DeleteContact.java
+++ src/FunctionDefinition/DeleteContact.java
@@ -2,6 +2,7 @@ package FunctionDefinition;
 
 
 import Linkedlist.LinkedListDefinitionClass;
+import Person.Person;
 
 import java.util.Scanner;
 
@@ -10,3 +11,7 @@ public class deleteContact {
     public void removeContactFormLIst(LinkedListDefinitionClass person) {
         Scanner tell = new Scanner(System.in);
         System.out.println("Here are all your contacts:");
+        for (int i = 0; i < person.getSize(); i++) {
+            Person person1 = (Person) person.dataAtIndex(i);
+            System.out.println(i + 1 + ". " + person1.getFrsitName() + " " + person1.getLastName());
+        }

304437f0bab7cdaa09adc99bc77a4ae7d7f4accf
yashvirsingh05 <yashvir.singh_cs18@gla.ac.in>
2020-02-24 19:34:55 +0530

    tell-->object of Scanner Class

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -3,10 +3,14 @@ package execution;
 import Linkedlist.LinkedListDefinitionClass;
 import Person.Person;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         LinkedListDefinitionClass<Person> listContact = new LinkedListDefinitionClass<Person>();
-
+        boolean turnoff = false;
+        Scanner tell = new Scanner(System.in);
+        int a = 0;
 
     }
 }

e036f95408f08f8adc4640404a2d3adb656e06b3
yashvirsingh05 <yashvir.singh_cs18@gla.ac.in>
2020-02-24 19:47:03 +0530

    delete contact method

--- src/FunctionDefinition/DeleteContact.java
+++ src/FunctionDefinition/DeleteContact.java
@@ -15,3 +15,10 @@ public class deleteContact {
             Person person1 = (Person) person.dataAtIndex(i);
             System.out.println(i + 1 + ". " + person1.getFrsitName() + " " + person1.getLastName());
         }
+        System.out.println("Press the number against the contact to delete it: ");
+        int choice = Integer.parseInt(tell.nextLine());
+        Person person2 = (Person) person.dataAtIndex(choice - 1);
+        System.out.println(person2.getFrsitName() + "'s contact deleted from list!");
+        person.remove(choice - 1);
+    }
+

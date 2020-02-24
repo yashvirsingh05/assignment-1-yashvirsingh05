8878ebd61f70127f7c8996b5e3fa3ecc448a316b
yashvirsingh05 <yashvir.singh_cs18@gla.ac.in>
2020-02-24 19:48:49 +0530

    class search

--- src/FunctionDefinition/SearchContact.java
+++ src/FunctionDefinition/SearchContact.java
@@ -1,4 +1,15 @@
 package FunctionDefinition;
 
+import Linkedlist.LinkedListDefinitionClass;
+import Person.Person;
+
+import java.util.Scanner;
+
 public class SearchContact {
-}
+    public void seach(LinkedListDefinitionClass person) {
+        View view = new View();
+        Scanner tell = new Scanner(System.in);
+        LinkedListDefinitionClass<Person> matchedContact = new LinkedListDefinitionClass();
+        System.out.println("You could search for a contact from their first names:");
+        String element = tell.nextLine()
+    }

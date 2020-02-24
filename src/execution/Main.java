487af6a77fa50ace37c0fc383f8b324beab75cd4
yashvirsingh05 <yashvir.singh_cs18@gla.ac.in>
2020-02-24 19:34:15 +0530

    list-->object of linklist

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -1,7 +1,11 @@
 package execution;
 
+import Linkedlist.LinkedListDefinitionClass;
+import Person.Person;
+
 public class Main {
     public static void main(String[] args) {
+        LinkedListDefinitionClass<Person> listContact = new LinkedListDefinitionClass<Person>();
 
 
     }

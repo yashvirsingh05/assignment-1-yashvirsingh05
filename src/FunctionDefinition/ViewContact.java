1ec57eaa7fb956626806472f7edc50c0ac0501e2
yashvirsingh05 <yashvir.singh_cs18@gla.ac.in>
2020-02-24 19:52:01 +0530

    method for view

--- src/FunctionDefinition/ViewContact.java
+++ src/FunctionDefinition/ViewContact.java
@@ -1,4 +1,13 @@
 package FunctionDefinition;
 
+import Linkedlist.LinkedListDefinitionClass;
+
 public class ViewContact {
+    public boolean viewList(LinkedListDefinitionClass person) {
+        System.out.println("--------------Here are all your Contacts---------");
+        int size = person.getSize();
+        printContact(person, size);
+        return true;
+    }
+
 }

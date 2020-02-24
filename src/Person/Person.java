68c4e81613efd5afe8c3cd8cfe2667c9a08bec71
yashvirsingh05 <yashvir.singh_cs18@gla.ac.in>
2020-02-24 20:27:02 +0530

    import linkedlistclass

--- src/Person/Person.java
+++ src/Person/Person.java
@@ -1,7 +1,10 @@
 package Person;
 
+import Linkedlist.LinkedListDefinitionClass;
+
 public class Person {
     private String frsitName;
     private String lastName;
+    private LinkedListDefinitionClass<Long> contactNo;
 
 }

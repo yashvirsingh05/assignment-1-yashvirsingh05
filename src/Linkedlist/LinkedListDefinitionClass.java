097a80b66744935a28316e046adcf02881e3195a
yashvirsingh05 <yashvir.singh_cs18@gla.ac.in>
2020-02-24 20:20:40 +0530

    make node and next

--- src/Linkedlist/LinkedListDefinitionClass.java
+++ src/Linkedlist/LinkedListDefinitionClass.java
@@ -125,3 +125,13 @@ public class LinkedListDefinitionClass {
 
     private static class Node<E> {
         private E data;
+        private Node<E> next;
+
+        private Node(E data) {
+            this.data = data;
+        }
+
+        private Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
